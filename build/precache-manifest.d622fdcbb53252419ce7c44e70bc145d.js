5937f9d8d3072a1be629996fb17b6a93c2591f32
Amy Barba <Amybarba70@gmail.com>
2019-09-11 23:26:02 -0700

    readme updated, project set up, npm dependencies updated

---
+++ build/precache-manifest.d622fdcbb53252419ce7c44e70bc145d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0017b576ac7ac9cd5d60cde84e0e9049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4fd80ca0b0041d000a",
+    "url": "/static/css/main.c4ad195c.chunk.css"
+  },
+  {
+    "revision": "426caf60e72190a4e2c9",
+    "url": "/static/js/2.46530864.chunk.js"
+  },
+  {
+    "revision": "2b4fd80ca0b0041d000a",
+    "url": "/static/js/main.3ff0a2e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
