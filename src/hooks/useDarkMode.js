7e9602e583236b56aad0234676a25594f948cf93
Amy Barba <Amybarba70@gmail.com>
2019-09-14 15:21:36 -0700

    debug darkmode file

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode  = () => {
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
+   
+    useEffect(() => {
+        debugger;
+        const body = document.querySelector('body');
+        if (darkMode) {
+            body.classList.add('dark-mode');
+        } else {
+            body.classList.remove('dark-mode');
+        }
+    }, [darkMode]);
+    return [darkMode, setDarkMode]
+};
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,13 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-export const  useLocalStorage = (key, initialValue) => {
-    const item = JSON.parse(window.localStorage.getItem(key));
-    
-    const [storedValue, setStoredValue] = useState(item || initialValue);
+// build a function called useLocalStorage
+// add key and initialValue as parameters for the hook
 
-    const setValue = value => {
-        window.localStorage.setItem(key, JSON.stringify(value));
-        setStoredValue(value);
-    };
-    return [storedValue, setValue];
+export const useLocalStorage = (key, initialValue) => {
+  // set up state called storedValue
+   const item = JSON.parse(window.localStorage.getItem(key));
+  const [storedValue, setStoredValue] = useState(item || initialValue);
+
+  const setValue = value => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+    setStoredValue(value);
+  }
+  return [storedValue, setValue];
 };
+
+// Written in response to the readme instructions
+// code not as clean as above
+// import React, { useState } from "react";
+
+// export const useLocalStorage = (key, initialValue) => {
+//      const [storedValue, setStoredValue] = useState(() => {
+// const item = window.localStorage.getItem(key);
+// return item ? JSON.parse(item) : initialValue;
+//      });
+//  const setValue = value => {
+//     window.localStorage.setItem(key, JSON.stringify(value));
+//     setStoredValue(value);
+//   }
+//   return [storedValue, setValue];
+// };
